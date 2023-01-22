cac4bd9fa22d44743e2d58afaa4bc09d01a42dc2
Eddy Varela <eddyvarela@eddys-mbp.mynetworksettings.com>
2023-01-22 17:01:40 -0500

    git push heroku main

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
+# import self-
 
 # To add a new path, first import the app:
 # import blog
@@ -15,7 +15,8 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+    # path("", hello.views.index, name="index"),
+    # path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    # path("self_storage_management/", include('self_storage_management.urls'))
 ]
